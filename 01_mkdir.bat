3401acdb1e285226c7d1b694e87349e5b30566d4
Austriaeste <32485171+Austriaeste@users.noreply.github.com>
2017-10-15 20:38:18 +0900

    Update 01_mkdir.bat

--- 01_mkdir.bat
+++ 01_mkdir.bat
@@ -1,19 +1,19 @@
 @echo off
 
-set share_dir=%USERPROFILE%\Desktop\pwd\
-set local_dir=%USERPROFILE%\Desktop\
+set SHARE_DIR=%USERPROFILE%\Desktop\pwd\
+set LOCAL_DIR=%USERPROFILE%\Desktop\
 
 @rem 空のフォルダとログ取得用テキストを作成
 set /P STR_INPUT="作成するフォルダ名を入力してください： "
-mkdir %local_dir%%STR_INPUT%\log\
-pushd %local_dir%%STR_INPUT%\log\
+mkdir %LOCAL_DIR%%STR_INPUT%\log\
+pushd %LOCAL_DIR%%STR_INPUT%\log\
 echo off > grep.txt
 echo off > vrf.txt
 
 @rem インプット用資材を格納する
-pushd %local_dir%
-copy %share_dir%ps.xlsx %local_dir%%STR_INPUT%
-start %local_dir%%STR_INPUT%
+pushd %LOCAL_DIR%
+copy %SHARE_DIR%ps.xlsx %LOCAL_DIR%%STR_INPUT%
+start %LOCAL_DIR%%STR_INPUT%
 
 exit
 
