431d8ad922e11dee944bf6bc5fb7e51af05d405b
Austriaeste <32485171+Austriaeste@users.noreply.github.com>
2017-11-03 11:02:22 +0900

    Update 03_SELECT-filepass.bat

--- 03_SELECT-filepass.bat
+++ 03_SELECT-filepass.bat
@@ -8,6 +8,7 @@ set /p user_input=please input month(ex:10)：
 pushd %shared_dir%%user_input%\
 start %shared_dir%%user_input%\
 
+rem 指定のパスがなければ、手動でその階層に特定のフォルダを作る
 if not exit %shared_dir%%user_input%\ start %shared_dir%%user_input%\
 
 exit
