3783de5cdf2276621fc3c9f28d6769574a905fa0
Austriaeste <32485171+Austriaeste@users.noreply.github.com>
2017-11-27 22:33:11 +0900

    Add files via upload

---
+++ 06_yyyymmdd.bat
@@ -0,0 +1,13 @@
+﻿@echo off
+
+rem クリップボードビューアを起動
+cd C:\Program Files (x86)\ShoClipboardViewer
+start ShoClipboardViewer
+
+rem 日付を取得
+set YYYYMMDD=%DATE:/=%
+
+rem 日付を出力
+set /P DUMMY="%YYYYMMDD%"<NUL|clip
+
+exit
